2eda3612cf72d18ef1b5868f1c9d637d7c5ee7f4
adilaroisa <adilaroisa688@gmail.com>
2025-10-24 09:29:09 +0700

    Membuat fungsi Composable menggunakan Column, Text, dan Spacer.

---
+++ app/src/main/java/com/example/pertemuan4/UIPertama.kt
@@ -0,0 +1,28 @@
+package com.example.pertemuan4
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun ActivitasPertama(modifier: Modifier){
+    Column(
+        modifier = Modifier
+            .padding(top = 100.dp)
+            .fillMaxsize()
+                horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = stringResource(id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(id = R.string.univ),
+            fontSize = 22.sp
+        )
+        Spacer(modifier = Modifier.height( height = 25.dp))
+    }
+}
+
