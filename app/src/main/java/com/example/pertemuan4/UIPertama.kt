3bd6aa519b055c64ce04de0d44f1369bb993d8b1
adilaroisa <adilaroisa688@gmail.com>
2025-10-24 10:10:17 +0700

    memanggil resources nama

--- app/src/main/java/com/example/pertemuan4/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4/UIPertama.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -15,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
@@ -68,9 +71,10 @@ fun ActivitasPertama(modifier: Modifier){
                             )
                             Text(
                                 text = stringResource(id = R.string.alamat),
-
+                                fontsize = 20.sp,
+                                color = Color.Yellow,
+                                modifier = Modifier.padding(top = 10.dp)
                                 )
-
                 }
 
 
