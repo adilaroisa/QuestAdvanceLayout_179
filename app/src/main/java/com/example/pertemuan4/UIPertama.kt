e212615256cd1a2c7acec9eaa762bd6850d4c59f
adilaroisa <adilaroisa688@gmail.com>
2025-10-24 10:19:57 +0700

    menambahkan modifier

--- app/src/main/java/com/example/pertemuan4/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4/UIPertama.kt
@@ -83,6 +83,7 @@ fun ActivitasPertama(modifier: Modifier){
                             ) {
                                 Text(
                                     text = stringResource(id = R.string.copy),
+                                    modifier = Modifier
 
                                 )
                             }
