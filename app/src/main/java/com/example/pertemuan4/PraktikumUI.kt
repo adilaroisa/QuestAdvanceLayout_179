a68ffa87c0af714d39acdf756175aad0953c237e
adilaroisa <adilaroisa688@gmail.com>
2025-10-25 20:01:11 +0700

    update menghapus yang tidak perlu

--- app/src/main/java/com/example/pertemuan4/PraktikumUI.kt
+++ app/src/main/java/com/example/pertemuan4/PraktikumUI.kt
@@ -21,7 +21,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.pertemuan4.R
+
 
 @Composable
 fun ProfileScreen(modifier: Modifier = Modifier) {
